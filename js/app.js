4928d49fa39ac4510c448a1da41013e8b8a0e1f9
Alberto Berroteran <aberroteran@live.com>
2014-05-24 00:50:42 -0430

    Router para el inicio

--- js/app.js
+++ js/app.js
@@ -5,10 +5,31 @@ App.render   = function(id, data){
     $('#output').html(rendered(data));
 }
 
-routie('', function() {
+$(function(){
     console.info('Default');
     $.getJSON('data.json', function(data){
-        App.data = data;
-        App.render('#default', {user: data});
+        App.data = data;    
     });
+});
+
+routie('', function() {
+    App.render('#default', {user: App.data});
+});
+
+routie('mapa/:id', function(e) {
+    var row = _.findWhere(App.data, {id: parseInt(e)});
+    App.render('#location', row);
+    map = new GMaps({
+        div: '#maps',
+        lat: row.latitude,
+        lng: row.longitude,
+        width: "100%",
+        height: "30em",
+        zoom : 5
+    });
+});
+
+routie('amigos/:id', function(e) {
+    var row = _.findWhere(App.data, {id: parseInt(e)});
+    App.render('#friend', row);
 });
